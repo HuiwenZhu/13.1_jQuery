28c9d195324809618a5c88005f49eec2c4a4cf58
HuiwenZhu <43683067+HuiwenZhu@users.noreply.github.com>
2021-08-18 22:22:33 -0700

    Add files via upload

---
+++ Practice/movieApp.js
@@ -0,0 +1,32 @@
+let currentId=0;
+
+let moviesList=[];
+
+$(function(){
+    $("#new-movie-form").on("submit",function(evt){
+        evt.preventDefault();
+        let title =$("#title").val();
+        let rating =$("#rating").val();
+
+        let movieData={title,rating,currentId};
+        const HTMLtoAppend = createMovieDataHTML(movieData);
+
+        currentId++
+        moviesList.push(movieData);
+
+        $("#movie-table-body").append(HTMLtoAppend);
+        $("new-movie-form").trigger("reset");
+    });
+ $("tbody").on("click",".btn.btn-danger",function(evt){
+     
+    let indexToRemoveAt=moviesList.findIndex(movie=> movie.currentId===+$(evt.target).data("deleteId"))
+
+    moviesList.splice(indexToRemoveAt,1)
+
+    $(evt.target)
+    .closet("tr")
+    .remove();
+ });
+
+ 
+})
